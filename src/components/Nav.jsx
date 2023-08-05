0f98abd91b5afce19217f8544621b98565448ae5
fran sola <fmia1992@gmail.com>
2023-08-05 20:09:53 +0200

    titulo nav redirige a home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { CatIcon, HeartIcon } from "./Icons";
 import { useMataGatos } from "../context/mataGatosContext";
 import { useFavouritesOwners } from "../context/favouritesOwnersContext";
+import { Link } from "wouter";
 
 export function Nav ({ title, showFavoritesModal }) {
     const { killedCats } = useMataGatos();
@@ -13,9 +14,11 @@ export function Nav ({ title, showFavoritesModal }) {
                 <CatIcon className="h-8 w-8 mr-2"/>
                 <span className="text-lg font-bold">{killedCats}</span>
             </div>
+            <Link href="/">
             <h1 className='text-2xl font-pacifico'>
                 {title}
             </h1>
+            </Link>
             <div className="flex gap-1 items-center" onClick={showFavoritesModal}>
                 <HeartIcon className="h-8 w-8 mr-2" />
                 <span className="text-lg font-bold"> {favouritesOwnersCount}</span>
