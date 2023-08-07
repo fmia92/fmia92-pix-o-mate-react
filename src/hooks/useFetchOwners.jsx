3094106faff673d16a144aff5a3b0e6bc89d0b46
fran sola <fmia1992@gmail.com>
2023-08-08 00:36:33 +0500

    fetch owners separado a una constamte

--- src/hooks/useFetchOwners.jsx
+++ src/hooks/useFetchOwners.jsx
@@ -17,53 +17,63 @@ export function useFetchOwners({ searchText }) {
     const [page, setPage] = useState(1);
     const [hasMoreData, setHasMoreData] = useState(true);
     const [loadingMoreData, setLoadingMoreData] = useState(false);
-                
-    useEffect(() => {
-        console.log({page})
-        console.log("useFetchOwners");
-        const url = searchText 
-            ? `https://gorest.co.in/public/v2/users?name=${searchText}&page=${page}&per_page=10`
-            : `https://gorest.co.in/public/v2/users?page=${page}&per_page=10`
 
+    // Verificamos si es Lunes o Jueves a las 12 de la noche
+    // const isUpdateDay = () => {
+    //     const now = new Date();
+    //     const day = now.getDay();
+    //     const hour = now.getHours();
+    //     return (day === 1 || day === 4) && hour === 0;
+    // };
+
+    const fetchOwners = ({searchText, page }) => {
+        const url = searchText 
+        ? `https://gorest.co.in/public/v2/users?name=${searchText}&page=${page}&per_page=10`
+        : `https://gorest.co.in/public/v2/users?page=${page}&per_page=10`
+
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json, charset=UTF-8",
+                "Authorization": "Bearer 61cb4c2c7bf7096f606410c9f450b345f49b62dad96126720505faefd448f350"
+            }
+        })
+        .then((response) => {
+            increaseKilledCats();
+            if (!response.ok) {
+                throw new Error("Algo salió mal...");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.length > 0) {
+                data.map((owner) => {
+                    owner.created_at = generateRamdomTimestamp();
+                });
+                setOwners((prev => page > 1 ? [...prev, ...data] : data));
+            } else {
+                setHasMoreData(false);
+            }
+                
+            setLoading(false);
+            setLoadingMoreData(false);
+        })
+        .catch((error) => {
+            setLoading(false);
+            setLoadingMoreData(false);
+            console.log(error)
+        });
+    };
+
+    useEffect(() => {
         if (loadingMoreData) return;
         setLoadingMoreData(true);
 
         if (!loadingMoreData && hasMoreData) {
-            fetch(url, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json, charset=UTF-8",
-                    "Authorization": "Bearer 61cb4c2c7bf7096f606410c9f450b345f49b62dad96126720505faefd448f350"
-                }
-            })
-            .then((response) => {
-                increaseKilledCats();
-                if (!response.ok) {
-                    throw new Error("Algo salió mal...");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                if (data.length > 0) {
-                    data.map((owner) => {
-                        owner.created_at = generateRamdomTimestamp();
-                    });
-                    setOwners((prev => page > 1 ? [...prev, ...data] : data));
-                } else {
-                    setHasMoreData(false);
-                }
-                    
-                setLoading(false);
-                setLoadingMoreData(false);
-            })
-            .catch((error) => {
-                setLoading(false);
-                setLoadingMoreData(false);
-                console.log(error)
-            });
-        }
+            fetchOwners({ searchText, page });
+        }   
     }, [page, searchText]);
-
+    
     return {
         owners,
         loading,
