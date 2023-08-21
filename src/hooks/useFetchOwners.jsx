297102c4f24d4cfe95ae765f94cf4dace429481d
fran sola <fmia1992@gmail.com>
2023-08-21 21:20:32 +0200

    useFecth owners usa react-query

--- src/hooks/useFetchOwners.jsx
+++ src/hooks/useFetchOwners.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useMataGatosStore } from '../context/mataGatosContext'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
 const generateRamdomTimestamp = () => {
   const from = new Date('2020-01-01').getTime()
@@ -9,140 +10,105 @@ const generateRamdomTimestamp = () => {
   return random
 }
 
-export function useFetchOwners({ searchText }) {
-  const [owners, setOwners] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(1)
-  const [hasMoreData, setHasMoreData] = useState(true)
-  const [loadingMoreData, setLoadingMoreData] = useState(false)
-
-  //toDo: Convertir a zustand
-  const increaseKilledCats = useMataGatosStore((state) => state.increaseKilledCats)
+const generateRandomPhoneNumber = () => {
+  const areaCode = '+' + Math.floor(Math.random() * 100).toString().padStart(2, '0');
+  const lineNumber = Math.floor(Math.random() * 1000000000).toString().padStart(9, '0');
+  return `${areaCode} ${lineNumber}`;
+}
 
   // Verificamos si es Lunes o Jueves a las 12 de la noche
-  const isUpdateDay = () => {
+const isUpdateDay = () => {
+    const now = new Date();
+    const day = now.getDay();
+    const hour = now.getHours();
+    return (day === 1 || day === 4) && hour === 0;
+};
+
+let cache = {};
+
+if (localStorage.getItem('cache')) {
+  cache = JSON.parse(localStorage.getItem('cache'));
+}
+
+async function fetchOwners({ pageParam = 1, searchText }) {
+  const url = searchText
+    ? `https://gorest.co.in/public/v2/users?page=${pageParam}&per_page=10&name=${searchText}`
+    : `https://gorest.co.in/public/v2/users?page=${pageParam}&per_page=10`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json, charset=UTF-8',
+      Authorization: 'Bearer 61cb4c2c7bf7096f606410c9f450b345f49b62dad96126720505faefd448f350',
+    },
+  });
+
+  const data = await response.json();
+
+  const owners = data.map((owner) => {
+    const createdAt = cache[`created_at_${owner.id}`];
+    const phone = cache[`phone_${owner.id}`];
+    owner.created_at = createdAt ? parseInt(createdAt) : generateRamdomTimestamp();
+    owner.phone = phone ? phone : generateRandomPhoneNumber();
+    return owner;
+  });
+
+  return owners;
+}
+
+export const useFetchOwners = ({ searchText }) => {
+  const { increaseKilledCats } = useMataGatosStore();
+
+  const { data, isLoading, error, isFetching, hasNextPage, fetchNextPage, refetch } = useInfiniteQuery(
+    ['owners', {searchText}],
+    ({ pageParam }) => fetchOwners({ pageParam, searchText }),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.length === 0) return;
+        return allPages.length + 1
+      },
+      onSuccess: () => {
+        increaseKilledCats();
+      },
+      refetchOnWindowFocus: false
+    }
+  )
+
+  
+  const owners = data?.pages?.flatMap((page) => page) ?? []
+
+  console.log('owners', owners)
+
+  useEffect(() => {
+    owners.forEach((owner) => {
+      if (!cache[`created_at_${owner.id}`]) {
+        cache[`created_at_${owner.id}`] = owner.createdAt;
+      }
+      if (!cache[`phone_${owner.id}`]) {
+        cache[`phone_${owner.id}`] = owner.phone;
+      }
+    });
+    localStorage.setItem('cache', JSON.stringify(cache));
+  }, [owners]);
+
+  useEffect(() => {
+    const dayInterval = setInterval(() => {
       const now = new Date();
       const day = now.getDay();
       const hour = now.getHours();
-      return (day === 1 || day === 4) && hour === 0;
-  };
-
-  const fetchOwners = ({searchText, page }) => {
-    const url = searchText 
-      ? `https://gorest.co.in/public/v2/users?name=${searchText}&page=${page}&per_page=10`
-      : `https://gorest.co.in/public/v2/users?page=${page}&per_page=10`
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json, charset=UTF-8',
-        'Authorization': 'Bearer 61cb4c2c7bf7096f606410c9f450b345f49b62dad96126720505faefd448f350'
+      if ((day === 1 || day === 4) && hour === 0) {
+        refetch();
       }
-    })
-      .then((response) => {
-        increaseKilledCats()
-        if (!response.ok) {
-          throw new Error('Algo salió mal...')
-        }
-        return response.json()
-      })
-      .then((data) => {
-        if (data.length > 0) {
-          data.map((owner) => {
-            owner.created_at = generateRamdomTimestamp()
-          })
-          setOwners((prev => page > 1 ? [...prev, ...data] : data))
-        } else {
-          setHasMoreData(false)
-        }
-                
-        setLoading(false)
-        setLoadingMoreData(false)
-      })
-      .catch((error) => {
-        console.log(error)
-        setLoading(false)
-        setLoadingMoreData(false)
-      })
-  }
-
-  useEffect(() => {
-    if (loadingMoreData) return
-    setLoadingMoreData(true)
-
-    if (!loadingMoreData && hasMoreData) {
-      fetchOwners({ searchText, page })
-    }   
-  }, [page, searchText])
-    
+    }, 60 * 60 * 1000);
+    return () => clearInterval(dayInterval);
+  }, [refetch]);
+  
   return {
+    isFetching,
     owners,
-    loading,
-    setPage,
-    hasMoreData,
-    setHasMoreData
+    error,
+    isLoading,
+    hasNextPage,
+    fetchNextPage
   }
 }
-
-//ToDo: Conertir a react-query
-// import { useQuery } from 'react-query';
-
-// const fetchOwners = async ({searchText, numPage = 1 }) => {
-//   const url = searchText 
-//     ? `https://gorest.co.in/public/v2/users?name=${searchText}&page=${page}&per_page=10`
-//     : `https://gorest.co.in/public/v2/users?page=${page}&per_page=10`
-
-//   const response = await fetch(url, {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json, charset=UTF-8',
-//       'Authorization': 'Bearer 61cb4c2c7bf7096f606410c9f450b345f49b62dad96126720505faefd448f350'
-//     }
-//   })
-
-//   if (!response.ok) {
-//     throw new Error('Algo salió mal...')
-//   }
-
-//   const data = await response.json()
-//   if (data.length > 0) {
-//     data.map((owner) => {
-//       owner.created_at = generateRamdomTimestamp()
-//     })
-//   } else {
-//     setHasMoreData(false)
-//   }
-//   return data
-// }
-
-//  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } = useInfiniteQuery(['owners', { searchText, page }], fetchOwners, {
-//     getNextPageParam: (lastPage) => {
-//       if (lastPage.length === 0) {
-//         return undefined
-//       }
-//       return page + 1
-//     }
-//   })
-
-// const handleScroll = useCallback(() => {
-//   if (!isLoading && hasNextPage) {
-//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 400) {
-//       fetchNextPage()
-//     }
-//   }
-// }, [hasNextPage, isLoading, fetchNextPage])
-//   useEffect(() => {
-//     if (data) {
-//       const owners = data.pages.flat()
-//       setOwners(owners)
-//     }
-//   }, [data])
-
-//   return {
-//     owners,
-//     isFetchingNextPage,
-//     loading: isLoading,
-//     hasMoreData: hasNextPage,
-//     setPage: fetchNextPage,
-//     handleScroll
-//   }
